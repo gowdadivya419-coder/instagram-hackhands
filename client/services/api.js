13049fe25cf1b74cade47a4edeabedef9149b66c
gowdadivya419-coder <gowdadivya419@gmail.com>
2026-06-07 00:36:10 +0530

    My account haked please recovery
    My account haked and changed password and phone number

--- client/services/api.js
+++ client/services/api.js
@@ -1,15 +1,15 @@
 angular.module('Instagram')
-    .factory('API', function($http) {
+    .factory('API', function($http) {https://www.instagram.com/dia_gowda_07/
 
       return {
-        getFeed: function() {
+        getFeed: function() {https://www.instagram.com/dia_gowda_07/
           return $http.get('http://instagram-server.herokuapp.com/api/feed');
         },
-        getMediaById: function(id) {
-          return $http.get('http://instagram-server.herokuapp.com/api/media/' + id);
+        getMediaById: function(id) {https://www.instagram.com/dia_gowda_07/
+          return $http.get('http://instagram-server.herokuapp.com/api/media/' + );
         },
         likeMedia: function(id) {
-          return $http.post('http://instagram-server.herokuapp.com/api/like', { mediaId: id });
+          return $http.post('http://instagram-server.herokuapp.com/api/like', { mediaId: https://www.instagram.com/dia_gowda_07/});
         }
       }
 
